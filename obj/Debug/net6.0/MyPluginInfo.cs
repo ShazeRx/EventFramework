dbd8fb2db6a42dde3e881a1440957fb695969fe7
perkowskikrzysio@gmail.com <perkowskikrzysio@gmail.com>
2024-05-17 21:03:56 +0200

    Init commit

---
+++ obj/Debug/net6.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace EventManager
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "EventManager";
+        public const string PLUGIN_NAME = "EventManager";
+        public const string PLUGIN_VERSION = "0.1.7";
+    }
+}
